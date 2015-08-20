84aea56e5e0bd033f8aa016feb90d8af346de31f
TheNotary <no@email.plz>
2015-08-20 10:46:41 -0500

    using @command_cache_fast_evaluator 2x performance boost

--- test/test_trtl.rb
+++ test/test_trtl.rb
@@ -19,6 +19,50 @@ describe Trtl do
 
   end
 
+  it "does easy example" do
+    pending
+    t = Trtl.new
+
+    10.times do
+      t.left(24)
+      t.forward(30)
+      t.dot
+      t.ensure_drawn
+    end
+
+    gets
+  end
+
+  it "draws the tree" do
+    Trtl.new(:width => 2, :color => 'brown').run do
+      def tree(_size)
+        if _size < 10
+          forward _size; back _size; return
+        end
+        color 'brown'
+        forward _size / 3
+        color %w{green darkgreen darkolivegreen}.sample
+        left 30; tree _size * 2 / 3; right 30
+        forward _size / 6
+        right 25; tree _size / 2; left 25
+        forward _size / 3
+        width rand(2) + 1
+        right 25; tree _size / 2; left 25
+        forward _size / 6
+        back _size
+        ensure_drawn
+      end
+
+      left 90
+      back 180
+      pen_down
+      tree 300.0
+
+      wait
+    end
+
+  end
+
   it "gets a default canvas when none is provided" do
     #@trtl.canvas.must_be_instance_of TkCanvas
   end
